9d034017340bd10f7e0a9daf2608f134e8327fe8
hkuzxc <3634729973@qq.com>
2026-04-17 20:12:16 +0800

    1

--- examples/models/qwen3_5/transformers.sh
+++ examples/models/qwen3_5/transformers.sh
@@ -1,30 +1,31 @@
-# 4 * 30GiB
+# 8卡 H20 141GB，Qwen3.5-397B-A17B
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
-NPROC_PER_NODE=4 \
+NPROC_PER_NODE=8 \
 IMAGE_MAX_TOKEN_NUM=1024 \
 VIDEO_MAX_TOKEN_NUM=128 \
 FPS_MAX_FRAMES=12 \
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 swift sft \
-    --model Qwen/Qwen3.5-35B-A3B \
+    --model /mnt/tidalfs-bdsz01/dataset/llm_ckpt/qwen3.5/Qwen3.5-397B-A17B \
     --tuner_type lora \
-    --dataset 'AI-ModelScope/LaTeX_OCR:human_handwrite#2000' \
+    --dataset 'swift/self-cognition#1000' \
     --load_from_cache_file true \
     --add_non_thinking_prefix true \
     --split_dataset_ratio 0.01 \
     --torch_dtype bfloat16 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
     --learning_rate 1e-4 \
     --lora_rank 8 \
     --lora_alpha 32 \
     --target_modules all-linear \
     --experts_impl grouped_mm \
     --router_aux_loss_coef 1e-3 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 16 \
+    --gradient_checkpointing true \
     --group_by_length true \
-    --output_dir output/Qwen3.5-35B-A3B \
+    --output_dir output/Qwen3.5-397B-A17B \
     --eval_steps 50 \
     --save_steps 50 \
     --save_total_limit 2 \
@@ -33,17 +34,4 @@ swift sft \
     --warmup_ratio 0.05 \
     --dataset_num_proc 4 \
     --dataloader_num_workers 4 \
-    --deepspeed zero3
-
-
-# PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
-# CUDA_VISIBLE_DEVICES=0,1,2,3 \
-# IMAGE_MAX_TOKEN_NUM=1024 \
-# VIDEO_MAX_TOKEN_NUM=128 \
-# FPS_MAX_FRAMES=12 \
-# swift infer \
-#     --adapters output/Qwen3.5-35B-A3B/vx-xxx/checkpoint-xxx \
-#     --stream true \
-#     --experts_impl grouped_mm \
-#     --enable_thinking false \
-#     --load_data_args true
+    --deepspeed zero3_offload
